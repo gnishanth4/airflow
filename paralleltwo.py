243a575f93338e75c20a75767776827dc483a8dc
Nishanth <gnishanth3@gmail.com>
2020-08-10 19:17:51 +0530

    Update paralleltwo.py

--- paralleltwo.py
+++ paralleltwo.py
@@ -1,5 +1,8 @@
 from airflow import DAG
 
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
 
 from datetime import datetime
 
